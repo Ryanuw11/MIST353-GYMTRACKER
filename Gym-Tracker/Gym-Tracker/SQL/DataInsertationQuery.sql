f41bf5100765eeba5ed96eacd5161ee631cb62b3
JoshuaMorrison11 <156019313+JoshuaMorrison11@users.noreply.github.com>
2024-10-03 20:19:21 -0400

    Update DataInsertationQuery.sql

--- Gym-Tracker/Gym-Tracker/SQL/DataInsertationQuery.sql
+++ Gym-Tracker/Gym-Tracker/SQL/DataInsertationQuery.sql
@@ -11,3 +11,22 @@ VALUES
 ('ReviveFitness', 'PortLand',  CAST('5:00' AS TIME),  CAST('19:00' AS TIME));
 GO
 
+-- Insert data into Membership table
+INSERT INTO Membership (ID,MembershipLevel,MembershipMonthPrice,MembershipMonthLength)
+
+VALUES
+('1','Bronze','2.99','12'),
+('2','Gold','9.99','12'),
+('3','Gold','9.99','8'),
+('4','Silver','7.99','9'),
+('5','Bronze','2.99','10'),
+('6','Silver','7.99','10'),
+('7','Gold','9.99','18'),
+('8','Bronze','2.99','12'),
+('9','Gold','9.99','8'),
+('10','Silver','7.99','6'),
+('11','Gold','9.99','6'),
+('12','Silver','7.99','10');
+
+
+GO
