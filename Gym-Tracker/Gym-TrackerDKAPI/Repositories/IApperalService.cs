48bbcec2e8f4152348cbfaa80f556dd2fa635b68
JoshuaMorrison11 <156019313+JoshuaMorrison11@users.noreply.github.com>
2024-12-05 11:11:29 -0500

    Pages cleanup working on first Razor page

--- Gym-Tracker/Gym-TrackerDKAPI/Repositories/IApperalService.cs
+++ Gym-Tracker/Gym-TrackerDKAPI/Repositories/IApperalService.cs
@@ -1,11 +1,10 @@
 ﻿using Gym_TrackerDKAPI.Data;
-using Gym_TrackerDKAPI.Entities;
 
 namespace Gym_TrackerDKAPI.Repositories
 {
     public interface IApperalService
     {
         Task<IEnumerable<ExtApperal>> ApperalGetAll(int apperal_id);
-      
+
     }
 }
